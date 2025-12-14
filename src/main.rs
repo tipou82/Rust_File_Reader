31ea211aa8b16bae2cba7953c7329a38cbb45e0d
Yunpeng Yang <yunpeng.yang@gmx.de>
2025-12-14 14:54:22 +0100

    changed provision of file name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::{self, Write};
 
 fn main() {
-    let file = File::open("existent_file.txt");
+    
+    print!("Enter filename: ");
+    io::stdout().flush().unwrap();
+    
+    let mut filename = String::new();
+    io::stdin().read_line(&mut filename).expect("Failed to read line");
+    let filename = filename.trim();
+    
+    let file = File::open(filename);
     let file = match file {
         Ok(file) => file,
         Err(error) => {
